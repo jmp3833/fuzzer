ba743a23810a0d24d8b989a32b0a9654aadd2e6d
Justin Peterson <jmp3833@rit.edu>
2015-09-25 15:12:33 -0400

    Add zombie js dependency

--- Discover/index.js
+++ Discover/index.js
@@ -1,4 +1,8 @@
 var urlParser = require('url');
+var browser = require('zombie');
+
+browser.localhost('127.0.0.1', 8080);
+browser.visit('/')
 
 module.exports = {
   discover: function(url, optionsMap) {
