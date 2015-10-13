d6ebc279888b0469e969ffb0c4654042aa49dda3
Keegan Parrotte <kmp3325@rit.edu>
2015-10-12 20:42:18 -0400

    Should be working.

--- Crawl/index.js
+++ Crawl/index.js
@@ -5,28 +5,40 @@ module.exports = {
   findPageLinks: function(browser, baseUrl, callback) {
     var stillLooking = 0;
     function crawl(baseUrl, discoveredUrls, mapping, url, document, callback) {
-      var foundPage = false;
+      console.log('Crawling ' + url);
       mapping[removeQueryParams(url)] = findInputs(url, document.forms);
       var links = [];
       for (var i = 0; i < document.links.length; i++) {
         links.push(document.links[i]);
       }
-      for (var i = 0; i < links.length; i++) {
-        if (inSameDomain(links[i], baseUrl)) {
-          var link = removeQueryParams(links[i].href);
-          if (discoveredUrls.indexOf(link) == -1) {
-            foundPage = true;
-            stillLooking++;
-            discoveredUrls.push(link);
 
-            browser.visit(links[i].href, function() {
-              crawl(baseUrl, discoveredUrls, mapping, browser.url, browser.document, callback);
-            });
-          }
+      if (links.length > 0) {
+        visitLinks(links, baseUrl, discoveredUrls, mapping, callback, 0);
+      } else if (stillLooking == 0) {
+        callback(mapping);
+      }
+    }
+
+    function visitLinks(links, baseUrl, discoveredUrls, mapping, callback, i) {
+      if (inSameDomain(links[i], baseUrl)) {
+        var link = removeQueryParams(links[i].href);
+        if (discoveredUrls.indexOf(link) == -1) {
+          stillLooking++;
+          discoveredUrls.push(link);
+
+          browser.visit(links[i].href, function () {
+            crawl(baseUrl, discoveredUrls, mapping, browser.url, browser.document, callback);
+            stillLooking--;
+            if (i < links.length) {
+              visitLinks(links, baseUrl, discoveredUrls, mapping, callback, i + 1);
+            } else if (stillLooking == 0) {
+              callback(mapping);
+            }
+          });
         }
       }
-      stillLooking--;
-      if (stillLooking == 0 && !foundPage) {
+
+      if (stillLooking == 0) {
         callback(mapping);
       }
     }
@@ -68,80 +80,3 @@ module.exports = {
     crawl(baseUrl, [], {}, browser.url, browser.document, callback);
   }
 }
-/*
- module.exports = {
- findPageLinks: function(browser, baseUrl, callback) {
- var mapping = {};
-
- var numPagesStillChecking = 0;
- baseUrl = baseUrl.replace('http://', '');
- if (baseUrl.indexOf(':') != -1) {
- baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf(':')); // Remove the port.
- }
- baseUrl = 'http://' + baseUrl;
-
- function crawl(endpoint, discoveredUrls, baseUrl, callback) {
- browser.visit(endpoint, function () {
- mapping[baseUrl + endpoint] = findInputs(browser, baseUrl + endpoint);
- numPagesStillChecking++;
- var foundPage = false;
- for (var i = 0; i< browser.document.links.length; i++) {
- console.log(browser.document.links[i].href);
- }
-
- for (var i = 0; i < browser.document.links.length; i++) {
- var url = urlParser.parse(normalizeUrl(browser.document.links[i].href));
-
- if (discoveredUrls.indexOf(removeQueryParams(url.href)) == -1 && compareDomains(url.href, baseUrl)) {
- foundPage = true;
- discoveredUrls.push(removeQueryParams(url.href));
-
- crawl(removeQueryParams(url.path), discoveredUrls, baseUrl, callback);
- }
- }
- numPagesStillChecking--;
- if (!foundPage && numPagesStillChecking == 0) { // if we are on a leaf and there are no threads still checking for new pages asynchronously.
- callback(mapping);
- }
- });
- }
-
- function compareDomains(url1, url2) {
- if (url1.startsWith('http://localhost') || url1.startsWith('http://127.0.0.1')) {
- return url2.startsWith('http://localhost') || url2.startsWith('http://127.0.0.1');
- } else if (url2.startsWith('http://localhost') || url2.startsWith('http://127.0.0.1')) {
- return url1.startsWith('http://localhost') || url1.startsWith('http://127.0.0.1');
- } else if (url1.startsWith(url2)) {
- return true;
- } else {
- return false;
- }
- }
-
- function removeQueryParams(url) {
- return url.split(/[?#]/)[0];
- }
-
- function findInputs(browser, urlObj) {
- var queryParams = urlObj.query == null ? [] : urlObj.query.split('&');
- for (var i = 0; i < queryParams.length; i++) {
- queryParams[i] = queryParams[i].split('=')[0];
- }
-
- var forms = browser.document.forms;
- var formParams = {};
- for (var i = 0; i < forms.length; i++) {
- var inputs = forms[i].getElementsByTagName('input');
- formParams[i] = [];
- for (var j = 0; j < inputs.length; j++) {
- formParams[i].push(inputs[j].getAttribute('name'));
- }
- }
- return {
- "query-params": queryParams,
- "form-params": formParams
- };
- }
- crawl('/', [], baseUrl, callback);
- }
- }*/
